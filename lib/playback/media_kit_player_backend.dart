8fa406ac3b35f294906aa20ca76982822c1beb56
Matthew Sigal <matthewsigal@gmail.com>
2026-07-11 12:47:05 -0700

    Simplify stream reconnection properties
    Incorporated review feedback

--- lib/playback/media_kit_player_backend.dart
+++ lib/playback/media_kit_player_backend.dart
@@ -381,7 +381,7 @@ class MediaKitPlayerBackend extends PlayerBackend {
       _nativeSetProperty(
         platform,
         'stream-lavf-o',
-        'reconnect=1,reconnect_on_network_error=1,reconnect_on_http_error=4xx,5xx,reconnect_streamed=1,reconnect_at_eof=1,reconnect_delay_max=5',
+        'reconnect=1,reconnect_on_network_error=1,reconnect_streamed=1,reconnect_delay_max=5',
       );
 
       final maxChannels = prefs.get(UserPreferences.maxAudioChannels);
